4b3e7864e3ef9ffb14cb441aec23fac757829f79
David Mojica <davidmojicav@gmail.com>
2023-11-04 13:29:50 -0500

    4-11-23 VI

--- main/static/js_resources/edit_account.js
+++ main/static/js_resources/edit_account.js
@@ -1,28 +1,28 @@
-// const btn_turn_180 = document.getElementById('btn_turn_180');
-// const turn_card = document.getElementById('id-turn-card');
-// const turn_card_inner = document.getElementById('id-turn-card-inner');
-// const turn_card_front = document.getElementById('turn-card-front');
-// const turn_card_back = document.getElementById('turn-card-back');
-// const btn_turn_0 = document.getElementById('btn-turn-0');
-// const form_split_id = document.getElementById('form-split-id');
-// const btn_ocultar_info = document.getElementById('btn_ocultar_info');
-// const btn_mostrar_info = document.getElementById('btn_mostrar_info');
-// const hidden_div = document.getElementById('hidden_div');
+const btn_turn_180 = document.getElementById('btn_turn_180');
+const turn_card = document.getElementById('id-turn-card');
+const turn_card_inner = document.getElementById('id-turn-card-inner');
+const turn_card_front = document.getElementById('turn-card-front');
+const turn_card_back = document.getElementById('turn-card-back');
+const btn_turn_0 = document.getElementById('btn-turn-0');
+const form_split_id = document.getElementById('form-split-id');
+const btn_ocultar_info = document.getElementById('btn_ocultar_info');
+const btn_mostrar_info = document.getElementById('btn_mostrar_info');
+const hidden_div = document.getElementById('hidden_div');
 
-// btn_turn_180.addEventListener('click', function(){
-//     turn_card_inner.style.transform = "rotateY(180deg)";
-// });
+btn_turn_180.addEventListener('click', function(){
+    turn_card_inner.style.transform = "rotateY(180deg)";
+});
 
-// btn_turn_0.addEventListener('click', function(){
-//     turn_card_inner.style.transform = "rotateY(0deg)";
-// });
+btn_turn_0.addEventListener('click', function(){
+    turn_card_inner.style.transform = "rotateY(0deg)";
+});
 
-// btn_mostrar_info.addEventListener('click', function(){
-//     form_split_id.style.display = "none";
-//     hidden_div.style.display = "grid";
-// });
+btn_mostrar_info.addEventListener('click', function(){
+    form_split_id.style.display = "none";
+    hidden_div.style.display = "grid";
+});
 
-// btn_ocultar_info.addEventListener('click', function(){
-//     form_split_id.style.display = "block";
-//     hidden_div.style.display = "none";
-// });
+btn_ocultar_info.addEventListener('click', function(){
+    form_split_id.style.display = "block";
+    hidden_div.style.display = "none";
+});
