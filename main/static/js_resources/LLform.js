3404c0d35e0492728e84c47534459c81e5519368
David Mojica <davidmojicav@gmail.com>
2023-11-30 12:11:27 -0500

    Client Validations LL I

---
+++ main/static/js_resources/LLform.js
@@ -0,0 +1,17 @@
+const form_llamadas = document.getElementById('form-llamadas'); //o
+const in_nombre = document.getElementById('in_nombre'); //o
+const in_documento = document.getElementById('in_documento');//o
+const in_tipo_documento = documeng.getElementById('in_tipo_documento');//o
+const in_sexo = document.getElementById('in_sexo'); //o
+const in_edad = document.getElementById('in_edad');
+const in_eps = document.getElementById('in_eps'); //o-n
+const in_direccion = document.getElementById('in_direccion');
+const in_pais = document.getElementById('in_pais');
+const in_departamento = document.getElementById('in_departamento');
+const in_municipio = document.getElementById('in_municipio');
+const in_telefono = document.getElementById('in_telefono');
+
+form_llamadas.addEventListener('submit', function(){
+
+});
+
