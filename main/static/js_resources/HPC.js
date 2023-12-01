da8bde0b67f4e712beff61109bd561eff808b91a
David Mojica <davidmojicav@gmail.com>
2023-12-01 12:26:33 -0500

    Hpc step-2 Client Validations II

--- main/static/js_resources/HPC.js
+++ main/static/js_resources/HPC.js
@@ -26,19 +26,3 @@ step0Form.addEventListener('submit', function(e){
         }, 4000);
     }
 });
-//step2 - Update
-const step1FormUpdate = document.getElementById('step1FormUpdate');
-const e_documento = document.getElementById('e_documento');
-const e_nombre = document.getElementById('e_nombre');
-const fecha_nacimiento = document.getElementById('fecha_nacimiento');
-const e_edad = document.getElementById('e_edad');
-
-fecha_nacimiento.addEventListener('change', function () {
-    var fechaNacimiento = new Date(fecha_nacimiento.value);
-    var fechaActual = new Date();
-    var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
-    if (fechaActual.getMonth() < fechaNacimiento.getMonth() || (fechaActual.getMonth() === fechaNacimiento.getMonth() && fechaActual.getDate() < fechaNacimiento.getDate())) {
-        edad--;
-    }
-    e_edad.value = edad;
-});
--- main/static/js_resources/HPCStep2.js
+++ main/static/js_resources/HPCStep2.js
@@ -1,9 +1,14 @@
 //step2 - Update
 const step1FormUpdate = document.getElementById('step1FormUpdate');
-const e_documento = document.getElementById('e_documento');
-const e_nombre = document.getElementById('e_nombre');
-const fecha_nacimiento = document.getElementById('fecha_nacimiento');
-const e_edad = document.getElementById('e_edad');
+const e_tipo_documento = document.getElementById('e_tipo_documento');
+const e_documento = document.getElementById('e_documento'); //o
+const e_nombre = document.getElementById('e_nombre'); //o 
+const fecha_nacimiento = document.getElementById('fecha_nacimiento'); //o
+const e_edad = document.getElementById('e_edad'); 
+const e_direccion = document.getElementById('e_direccion'); //oopc
+const e_barrio = document.getElementById('e_barrio'); //opc
+const e_hijos = document.getElementById('e_hijos'); //opc
+
 
 fecha_nacimiento.addEventListener('change', function () {
     var fechaNacimiento = new Date(fecha_nacimiento.value);
