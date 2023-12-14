b2203343812a338cc103ef982644ecb16a3c8471
David Mojica <davidmojicav@gmail.com>
2023-12-14 13:23:11 -0500

    14/12/23 Detalles paciente Layouting

--- main/views.py
+++ main/views.py
@@ -1913,6 +1913,19 @@ def detallespaciente(request):
             'paciente':pacienteInstance,
             'edadActual': edad,
             
+            'pobVulnerable': poblacion_vulnerable,
+            'epss': EPSS,
+            'escolaridad': escolaridades,
+            'estadoC':estados_civiles,
+            'etnia': etnias,
+            'lecto': lectoescritura1,
+            'lecto2': lectoescritura2,
+            'municipios':municipios,
+            'ocupaciones': ocupaciones,
+            'razonamientos': razonamiento,
+            'regimen':regimenes,
+            'sexos': sexos,
+            'tpDoc': tipos_documento
             })
             
         else:
