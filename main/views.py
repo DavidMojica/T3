ee5fe2121351413008f0cf8d8f7d7671171e53e6
David Mojica <davidmojicav@gmail.com>
2023-12-27 11:26:58 -0500

    PDF Data Set Up

--- main/views.py
+++ main/views.py
@@ -1961,6 +1961,7 @@ def not_deployed_404(request):
     if request.method == "GET":
         return render(request, '404_not_deployed.html')
     
+    
 @login_required
 def generar_pdf(request):
     data = ["cooco", "channel"]
